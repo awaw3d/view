d798c87ad5e94007059c54d34c194281bb833875
awaw3d <74543708+awaw3d@users.noreply.github.com>
2021-12-30 16:30:41 -0500

    Update button.js

--- js/render/nodes/button.js
+++ js/render/nodes/button.js
@@ -25,7 +25,7 @@ import {PrimitiveStream} from '../geometry/primitive-stream.js';
 //const BUTTON_SIZE = 0.1;
 //const BUTTON_CORNER_RADIUS = 0.025;
 const BUTTON_SIZE = 0.05;
-const BUTTON_CORNER_RADIUS = 0.0125;
+const BUTTON_CORNER_RADIUS = 0.025;
 const BUTTON_CORNER_SEGMENTS = 8;
 const BUTTON_ICON_SIZE = 0.07;
 const BUTTON_LAYER_DISTANCE = 0.005;
